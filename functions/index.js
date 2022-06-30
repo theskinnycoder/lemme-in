596ce9c7a438a1bd0db3557dd789a4cf1f117eac
theskinnycoder <18131a0505@gvpce.ac.in>
2022-06-30 18:05:48 +0530

    Updated Timimg

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,12 @@ const functions = require("firebase-functions");
 exports.submitForm = functions
   .region("asia-south1")
   // every weekday at 6:00 PM
-  .pubsub.schedule("0 6 * * 0-5")
+  .pubsub.schedule("30 6 * * 1-5")
   // Asia Kolkata
   .timeZone("Asia/Kolkata")
   .onRun(async (context) => {
     const res = await fetch("https://lemme-in.vercel.app/api/automated-form", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
     });
     const data = await res.json();
     functions.logger.log(data);
